3f7718d6b197dd93ad3681204cbb27a1fe969717
Marcus Hammarberg <marcusoft.net@gmail.com>
2013-12-06 15:19:06 +0100

    Smaller, tighter and better version

--- server/routes/quote.js
+++ server/routes/quote.js
@@ -6,22 +6,8 @@
 
 exports.createNewQuote = function (db) {
 	return function (req, res) {
-
-		var name = req.body.name;
-		var kidName = req.body.kidname;
-        var quote = req.body.text;
-        var saidAt = req.body.saidat;
-
-        console.log("About to add: " + kidName);
-        console.log("About to add: " + quote);
-
         var collection = db.get('quotes');
-
-        collection.insert({
-            "name" : kidName,
-            "quote" : quote,
-            "saidAt" : saidAt
-        }, function (err, doc) {
+        collection.insert(req.body, function (err, doc) {
             if (err) {
                 // If it failed, return error
                 res.send("There was a problem adding the information to the database.");
